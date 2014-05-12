ba4a23d7c2f9cb2bd9499e255c8ac81b47a0d601
Joseph Leeper <jdcordella@gmail.com>
2014-05-12 11:24:46 -0500

    Initial commit.

---
+++ little_helpers.cpp
@@ -0,0 +1,177 @@
+#include <little_helpers.h>
+
+
+float calc_input_voltage(int input)
+{
+    return input / 1023.0 * 5.0;
+}
+
+
+float calc_output_voltage(int output)
+{
+    return output / 255.0 * 5.0;
+}
+
+
+
+long dither(float f)
+{
+    /* Uses a simple coin flip to determine how to quantize the input. */
+
+    return (unsigned long) (f + random(2));
+}
+
+long dither(float f, word n)
+{
+    /* Uses a Gaussian dither to quantize the input. */
+    
+    unsigned long rolls = 0;
+
+    for (word i = 0; i < n; i++)
+        rolls += random(101);
+
+    return (unsigned long) (f + ((double) rolls / n - 50));   
+}
+
+
+
+void print_binary(word input, byte length)
+{
+    char temp_str1[17], temp_str2[17];
+    itoa(input, temp_str1, 2);
+    size_t length_diff = length - strlen(temp_str1);
+    memset(temp_str2, '0', length_diff);
+    temp_str2[length_diff] = '\0';
+    sprintf(temp_str2, "%s%s", temp_str2, temp_str1);
+    Serial.print(temp_str2);
+}
+
+void print_bool(boolean input)
+{
+    if (input == true)
+        Serial.print("TRUE");
+    else
+        Serial.print("FALSE");
+}
+
+void print_bool(boolean input, boolean is_digital)
+{
+    if (is_digital == true)
+    {
+        if (input == HIGH)
+            Serial.print("HIGH");
+        else
+            Serial.print("LOW");
+    }
+
+    else
+    {
+        if (input == HIGH)
+            Serial.print("TRUE");
+        else
+            Serial.print("FALSE");
+    }
+}
+
+void print_ordered_pair(boolean one, boolean two, boolean is_digital)
+{
+    Serial.print('(');
+    print_bool(one, is_digital);
+    Serial.print(", ");
+    print_bool(two, is_digital);
+    Serial.print(')');    
+}
+
+
+void print_ordered_pair(double one, double two)
+{
+    Serial.print('(');
+    Serial.print(one);
+    Serial.print(", ");
+    Serial.print(two);
+    Serial.print(')');    
+}
+
+
+void print_ordered_pair(double one, double two, byte precision)
+{
+    Serial.print('(');
+    Serial.print(one, precision);
+    Serial.print(", ");
+    Serial.print(two, precision);
+    Serial.print(')');    
+}
+
+void pulse_pin(byte pin, word milliseconds, boolean active_low)
+{
+    digitalWrite(pin, !active_low);
+    delay(milliseconds);
+    digitalWrite(pin, active_low);
+}
+
+// void pulse_pin(byte pin, float milliseconds, boolean active_low)
+// {
+//     digitalWrite(pin, !active_low);
+//     delayMicroseconds(milliseconds * 1000);
+//     digitalWrite(pin, active_low);
+// }
+
+/* https://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer */
+byte hamming_weight(unsigned long i)
+{
+    i = i - ((i >> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+    return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+}
+
+
+/* G2, G1, G0 */
+const byte LMP8358_p_t::GAIN_CODES[7] = {B000,  // 10x (power-up default) 930 kHz 000b
+                                         B001,  // 20x 385 kHz 000b
+                                         B010,  // 50x 460 kHz 001b
+                                         B011,  // 100x 640 kHz 010b
+                                         B100,  // 200x 195 kHz 010b
+                                         B101,  // 500x 130 kHz 011b
+                                         B110,  // 1000x 89 kHz 011b
+                                         // B111   // User defined 800 kHz 1xxb
+};
+
+
+void LMP8358_p_t::adjust_gain(gain_t gain)
+{
+    digitalWrite(G2, (GAIN_CODES[gain] >> 2) & 0x1);
+    digitalWrite(G1, (GAIN_CODES[gain] >> 1) & 0x1);
+    digitalWrite(G0, GAIN_CODES[gain] & 0x1);
+}
+
+gain_t LMP8358_p_t::auto_gain()
+{
+    const word SATURATION = 1015;
+    word input;
+    char i = 6;
+    gain_t gain = x1000;
+
+    adjust_gain(gain);
+
+    while (i > 0)
+    {
+        input = analogRead(OUT);
+        if (input >= SATURATION)
+        {
+            i--;
+            gain = (gain_t) i;
+            adjust_gain(gain);
+        }
+        else
+            break;
+    }
+
+    _gain = gain;
+
+    return gain;
+}
+
+gain_t LMP8358_p_t::get_gain() const
+{
+    return _gain;
+}
---
+++ little_helpers.h
@@ -0,0 +1,84 @@
+#ifndef LITTLE_HELPERS_H
+#define LITTLE_HELPERS_H
+
+
+#include <Arduino.h>
+
+
+struct HC595_t
+{
+    const byte SER, RCLK, SRCLK;
+
+    HC595_t(byte ser_pin, byte rclk_pin, byte srclk_pin)
+        :SER(ser_pin), RCLK(rclk_pin), SRCLK(srclk_pin)
+    {}
+};
+
+
+struct MBI5026_t
+{
+    const byte SDI, CLK, LE;
+
+    MBI5026_t(byte sdi_pin, byte clk_pin, byte le_pin)
+        :SDI(sdi_pin), CLK(clk_pin), LE(le_pin)
+    {}
+};
+
+
+struct HC597_t
+{
+    const byte _PL, STCP, SHCP, Q;
+
+    HC597_t(byte _pl_pin, byte stcp_pin, byte shcp_pin, byte q_pin)
+        :_PL(_pl_pin), STCP(stcp_pin), SHCP(shcp_pin), Q(q_pin)
+    {}
+};
+
+
+enum gain_t {x10, x20, x50, x100, x200, x500, x1000};
+
+
+struct LMP8358_p_t
+{
+    const byte OUT, G2, G1, G0;
+
+    LMP8358_p_t(byte out_pin, byte g2_pin, byte g1_pin, byte g0_pin, gain_t gain)
+        :OUT(out_pin), G2(g2_pin), G1(g1_pin), G0(g0_pin), _gain(gain)
+    {}
+
+    void adjust_gain(gain_t gain);
+    gain_t auto_gain();
+
+    gain_t get_gain() const;
+    
+
+private:
+    gain_t _gain;
+    const static byte GAIN_CODES[7];
+};
+
+
+
+
+float calc_input_voltage(int input);
+float calc_output_voltage(int output);
+
+long dither(float f);
+long dither(float f, word n);
+
+void print_binary(const word input, const byte length);
+
+void print_bool(boolean input);
+void print_bool(boolean input, boolean is_digital);
+
+void print_ordered_pair(boolean one, boolean two, boolean is_digital);
+void print_ordered_pair(double one, double two);
+void print_ordered_pair(double one, double two, byte precision);
+
+void pulse_pin(byte pin, word milliseconds, boolean active_low);
+// void pulse_pin(byte pin, float milliseconds, boolean active_low);
+
+byte hamming_weight(unsigned long i);
+
+
+#endif
